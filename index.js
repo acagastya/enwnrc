1fc8faf1198c0d01560452e296bdb994875b231c
Agastya Chandrakant <acagastya@outlook.com>
2020-07-27 04:47:02 +0530

    Being more verbose.

--- index.js
+++ index.js
@@ -78,8 +78,12 @@ eventSource.onmessage = function(event) {
           if (comment) msg += " " + comment;
         }
         // new account log
+        // to implement
+        // other log actions
         else ircClient.say("acagastya", JSON.stringify(change));
-      } else ircClient.say("acagastya", JSON.stringify(change));
+      }
+      // other actions
+      else ircClient.say("acagastya", JSON.stringify(change));
       ircClient.say(channel, msg);
     } catch (error) {
       msg = JSON.stringify(change);
