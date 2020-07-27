1b3396f2a0148e8950d0f4a5a0106a70118c1f3f
Agastya Chandrakant <acagastya@outlook.com>
2020-07-27 10:34:32 +0530

    implement colours

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const ES = require("eventsource");
 const irc = require("irc");
+const c = require("irc-colors");
 
 const { API, botName, channel, server, wiki } = require("./config");
 
 const ircClient = new irc.Client(server, botName, { channels: [channel] });
-ircClient.addListener("error", function(message) {
+ircClient.addListener("error", function (message) {
   console.log("error: ", message);
 });
 
 console.log("Connecting to the event stream...");
 const eventSource = new ES(API);
 
-eventSource.onopen = function(event) {
+eventSource.onopen = function (event) {
   console.log("--- Opened connection.");
 };
 
-eventSource.onerror = function(event) {
+eventSource.onerror = function (event) {
   ircClient.say("acagastya", JSON.stringify(event) + "\n --- error");
   console.error("--- Encountered error", event);
 };
 
-eventSource.onmessage = function(event) {
+eventSource.onmessage = function (event) {
   const change = JSON.parse(event.data);
   let msg = "";
   if (change.wiki == wiki) {
@@ -45,50 +46,68 @@ eventSource.onmessage = function(event) {
         const uri = `${server_url}${server_script_path}/index.php?diff=${revision["new"]}&oldid=${revision.old}`;
         const size = length["new"] - length["old"];
         const sign = size >= 0 ? "+" : "";
-        msg = `[[${title}]] `;
-        if (minor) msg += "M";
-        if (bot) msg += "B";
-        msg += ` ${uri} * ${user} * (${sign}${size})`;
-        if (comment) msg += ` ${comment}`;
+        msg = `[[${c.olive(title)}]] `;
+        if (minor) msg += `${c.red("M")}`;
+        if (bot) msg += `${c.red("B")}`;
+        msg += ` ${c.navy(uri)} ${c.maroon("*")} ${c.green(user)} ${c.maroon(
+          "*"
+        )} (${sign}${size})`;
+        if (comment) msg += ` ${c.teal(comment)}`;
       }
       // new page created action
       else if (type == "new") {
         const uri = `${server_url}${server_script_path}/index.php?oldid=${revision["new"]}&rcid=${id}`;
         const size = length["new"];
-        msg = `[[${title}]] ${
-          !patrolled ? "!" : ""
-        }N ${uri} * ${user} * (+${size})`;
-        if (comment) msg += ` ${comment}`;
+        msg = `[[${c.olive(title)}]] ${
+          !patrolled ? c.red("!") : ""
+          }c.red("N") ${c.navy(uri)} ${c.maroon("*")} ${c.green(user)} ${c.maroon(
+            "*"
+          )} (+${size})`;
+        if (comment) msg += ` ${c.teal(comment)}`;
       }
       // log actions
       else if (type == "log") {
         const { log_action, log_type } = change;
         // review log
         if (change.log_type == "review") {
-          msg = `[[Special:Log/review]] ${log_action} `;
-          if (bot) msg += "B";
-          msg += ` * ${user} *  ${log_action_comment}`;
+          msg = `[[${c.olive("Special:Log/review")}]] ${c.red(log_action)} `;
+          if (bot) msg += c.red("B");
+          msg += ` ${c.maroon("*")} ${c.green(user)} ${c.maroon("*")}  ${c.teal(
+            log_action_comment
+          )}`;
         }
         // delete log
         else if (log_type == "delete") {
-          msg = `[[Special:Log/delete]] ${log_action} `;
-          if (bot) msg += "B";
-          msg += ` * ${user} *  ${
-            log_action == "delete" ? "deleted" : "DID SOMETHING ELSE"
-          } "[[${title}]]"`;
-          if (comment) msg += " " + comment;
+          msg = `[[${c.olive("Special:Log/delete")}]] ${c.red(log_action)} `;
+          if (bot) msg += c.red("B");
+          msg += ` ${c.maroon("*")} ${c.green(user)} ${c.maroon("*")}  ${
+            log_action == "delete" ? c.teal("deleted") : "DID SOMETHING ELSE"
+            } "[[${c.blue(title)}]]"`;
+          if (comment) msg += " " + c.teal(comment);
         }
         // new account log
         else if (log_type == "newuser") {
-          msg = `[[Special:Log/newusers]] `;
-          if (bot) msg += "B";
-          msg += ` ${log_action}  * ${user} *  ${log_action_comment}`;
+          msg = `[[${c.olive("Special:Log/newusers")}]] `;
+          if (bot) msg += c.red("B");
+          msg += ` ${c.red(log_action)}  ${c.maroon("*")} ${c.green(
+            user
+          )} ${c.maroon("*")}  ${c.teal(log_action_comment)}`;
         }
         // block
         else if (log_type == "block") {
-          msg = `[[Special:Log/block]] ${log_action} `;
-          if (bot) msg += "B";
-          msg += ` * ${user} *  ${log_action_comment}`;
+          msg = `[[${c.olive("Special:Log/block")}]] ${c.red(log_action)} `;
+          if (bot) msg += c.red("B");
+          msg += ` ${c.maroon("*")} ${c.green(user)} ${c.maroon("*")}  ${c.teal(
+            log_action_comment
+          )}`;
+        }
+        // thank
+        else if (log_type == "thank") {
+          msg = `[[${c.olive("Special:Log/thanks")}]] ${c.red(
+            log_action
+          )}  ${c.maroon("*")} ${c.green(user)} ${c.maroon("*")}  ${c.teal(
+            log_action_comment
+          )}`;
         }
         // other log actions
         else ircClient.say("acagastya", JSON.stringify(change));
